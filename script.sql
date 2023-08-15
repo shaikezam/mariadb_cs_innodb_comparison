38bf82171e0174ca26e81a0a38e2bcf562f7ea75
Shai Zambrovski <szambrovski@proofpoint.com>
2023-08-15 15:33:06 +0300

    add quries

--- script.sql
+++ script.sql
@@ -18,13 +18,25 @@ CREATE TABLE person_inno(
 	stateId INT
 ) engine=InnoDB;
 
-CREATE TABLE states (
+CREATE TABLE states_inno (
     id INT NOT NULL,
     state TEXT NOT NULL
 ) ENGINE = InnoDB;
 
+CREATE TABLE states_cs (
+    id INT NOT NULL,
+    state TEXT NOT NULL
+) ENGINE = ColumnStore;
+
 LOAD DATA INFILE '/tmp/states.csv'
-INTO TABLE states
+INTO TABLE states_inno
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, state);
+
+LOAD DATA INFILE '/tmp/states.csv'
+INTO TABLE states_cs
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
@@ -81,10 +93,14 @@ VALUES
 
 CREATE INDEX idx_age ON person_inno(age);
 CREATE INDEX idx_state_id ON person_inno(stateId);
+CREATE INDEX idx_first_name ON person_inno(firstName);
 
 select count(*), age from person_inno group by age having age > 12 order by age asc;
 
-select count(*), state from person_inno p join states s on p.stateId = s.id where p.age > 31 group by s.state;
-select count(*), state from person_cs p join states s on p.stateId = s.id where p.age > 31 group by s.state;
+select count(*), state from person_inno p join states_inno s on p.stateId = s.id where p.age > 31 group by s.state order by s.state desc;
+select count(*), state from person_cs p join states_cs s on p.stateId = s.id where p.age > 31 group by s.state order by s.state desc;
+
+select count(*), state from person_cs p join states_cs s on p.stateId = s.id where p.age > 31 and firstName like '%o%' group by s.state order by s.state desc;
+select count(*), state from person_inno p join states_inno s on p.stateId = s.id where p.age > 31 and firstName like '%o%' group by s.state order by s.state desc;
 
 */
